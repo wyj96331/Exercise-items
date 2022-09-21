5d98e4d104dbde975055f45a5d76666ccf82402f
wyj714 <714624631@qq.com>
2022-09-22 05:42:15 +0800

    all

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  // 代理跨域
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://gmall-h5-api.atguigu.cn'
+        // pathRewrite: { '^/api': '' }
+      }
+    }
+  }
+})
